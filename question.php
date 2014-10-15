acbcf828ee62526c43a311f56e53ba804d062fd5
Shubham Lagwankar <shubhu105@gmail.com>
2014-10-16 00:04:59 +0530

    Record views for category

--- question.php
+++ question.php
@@ -1,6 +1,7 @@
 <?php
-$question = htmlspecialchars($_GET["question"]);
-$category = htmlspecialchars($_GET["category"]);
+session_start();
+$question = $_GET["question"];
+$category = $_GET["category"];
 
 if ($question == null or $category == null) { die("Question not found."); }
 $con = new mysqli("localhost", "devshubh", "", "qa");
@@ -12,6 +13,32 @@ $stm2->execute();
 $stm2->bind_result($qtext, $qdesc);
 $stm2->fetch();
 $stm2->close();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (! isset($_SESSION['user'])) {
+        /* Redirect to registration */
+    }
+    else {
+        /* Getting the user ID */
+        $user = $_SESSION['user'];
+        $stm3 = $con->prepare("SELECT UserId FROM user WHERE Username = ?");
+        $stm3->bind_param('s', $user);
+        $stm3->execute();
+        $stm3->bind_result($userID);
+        $stm3->fetch();
+        $stm3->close();
+
+        /* Adding the answer */
+        $answer_text = mysqli_real_escape_string($_POST['txtDesc']);
+        $stm4 = $con->prepare("INSERT INTO answers (answer_text,qid,UserId" .
+                ",name) VALUES (?,?,?,?)");
+        $stm4->bind_param('sdds', $answer_text, $question, $userID, $category);
+        $stm4->execute();
+
+        header('Location: ' . 'http://localhost/qa/question.php?category=' .
+                urlencode($category) . '&question=' . urlencode($question));
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -26,8 +53,11 @@ $stm2->close();
       <h2><?php echo htmlspecialchars($qtext); ?></h2>
       <hr>
     </div>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+	<textarea name="txtDesc" row="50" cols="50" id="txtDesc" wrap="hard"></textarea><br><br>
+	<input type="submit" value="Add answer" name="submit"><br><br>
+	</form>
     <?php
-
     $stm = $con->prepare("SELECT answer_id, answer_text  FROM answers WHERE name = ? AND qid = ?");
     $stm->bind_param('sd', $category, $question);
     $stm->execute();
