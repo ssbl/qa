0fcf05ea4d717847bbc5f13979c3c695cd2452fe
Codekrypt <cooldevansh_93@hotmail.com>
2014-10-16 03:12:47 +0530

    Initial design

--- home.php
+++ home.php
@@ -4,33 +4,74 @@ session_start();
 <!DOCTYPE html>
 <html>
   <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-    <title>QA</title>
+    <title>HOME | QA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=2.0" >
+    
+    <link href="css/bootstrap-fluid-adj.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
   </head>
 
   <body>
+<div class="alert alert-warning">
+    <a href="" class="close" data-dismiss="alert">&times;</a>
+    <strong>Hello!</strong>     Welcome to the website!
+</div>
+<div class="container">
+  <nav class="navbar navbar-default">
+       
+      <div class="container-fluid">
+        <div class="navbar-header" >
+          <a class="navbar-brand" >QA</a>
+        </div>
+        <div>
+	</div>
+	
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="home.php">Home</a></li>
+            <li><a href="Register.php">Register</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="About.php">About</a></li>
+          </ul>
+	  </div>
+	 
+	  
+	<div class="container">
+	<div class="jumbotron">
+	<img src="random.png" class="img-responsive" align="right" alt ="Logo" width="180" height="180">
+	     <div class="col-xs-6 col-md-4 col-md-offset-4">
     <h1>QA Homepage</h1>
+    
     <p>A simple question-and-answer website.</p>
     <p><?php echo '<em>Welcome, </em> ' . $_SESSION['user'] . '.'; ?></p>
     <hr>
+	</div>
+	</div>
+	</div>
     <form id="qinstant" action="addq.php" method="post">
-      <input type="text" name="qtext" placeholder="Ask away!">
+
+      <input type="text" name="qtext" placeholder="Ask away!" class="form-control">
+      <br>
       <?php
       $con = new mysqli("localhost", "devshubh", "", "qa");
       if ($con->connect_error) { die("Error connecting to DB: " . mysqli_error()); }
       $results = $con->query("SELECT * FROM category");
-      echo '<select name="category">';
+     
+      echo '<select name="category" class="form-control">';
       while ($row = $results->fetch_array()) {
           $category = $row["name"];
           echo "<option>" . $category . "</option>";
       }
       echo "</select>";
       ?>
-      <button type="submit" name="submit">Post question</button>
+     <br> <button type="submit" name="submit"class="btn btn-info btn-lg" >Post question</button>
     </form>
     <br>
 
-    <h3>Popular categories</h3>
+    <h3><span class="glyphicon glyphicon-star-empty"></span>Popular Categories</h3>
     <?php
     $con = new mysqli("localhost", "devshubh", "", "qa");
 
